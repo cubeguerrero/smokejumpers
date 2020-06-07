7ec48791c9ef6f90522a6537425af9bfba8496f0
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2020-06-07 17:33:56 +0800

    Generated Types::FederationType

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,11 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :federations,
+          [Types::FederationType],
+          null: false,
+          description: "Returns a list of federations in the database."
+    def federations
+      Federation.all
     end
   end
 end
